a131c865152744c5126889dafabf5b641a615864
Ricardo Avaca <ricardo.avaca@usach.cl>
2023-11-07 08:18:53 -0300

    Preparando archivos

---
+++ backend-eureka-service/src/test/java/cl/usach/backendeurekaservice/BackendEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.backendeurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend-getaway-service/src/main/java/cl/usach/backendgetawayservice/BackendGetawayServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.backendgetawayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGetawayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGetawayServiceApplication.class, args);
+	}
+
+}
